86d8ea6c9faf5c2a506e70ee659ab8876c8c7735
Martin Wanczyk <wanczyk.martin@gmail.com>
2020-09-07 17:00:34 +0200

    Initial commit

---
+++ Assets/test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter (Collision collision)
+    {
+        if(collision.transform.gameObject.tag == "Curb")
+        {
+            Debug.Log("collision");
+        }
+    }
+    void OnCollisionExit (Collision collision)
+    {
+        if(collision.transform.gameObject.tag == "Curb")
+        {
+            Debug.Log("exit collision");
+        }
+    }
+}
